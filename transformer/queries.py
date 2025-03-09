1ea351d2431cc79374f4431e4c34cc6bf57fefa2
Mikhail Kaluzhnyy <teviroff@gmail.com>
2025-03-10 01:57:18 +0300

    tweaks to tags query & raw form query

--- transformer/queries.py
+++ transformer/queries.py
@@ -13,12 +13,20 @@ def get_schema_file(filename: str):
 
 SCHEMAS = {
     'tags': get_schema_file('categorized-tags.schema.json').read(),
+    'form': get_schema_file('form.schema.json').read(),
 }
 
 QUERY_MESSAGES = {
     'tags.system': (
         'You will be provided with HTML page of an opportunity, and your task is to parse '
-        f'it according to this JSON schema: \n{SCHEMAS["tags"]}\nMake sure that you respond in English'
+        f'it according to this JSON schema: {SCHEMAS["tags"]}.\n'
+        "If you can't fill some fields from given HTML, you don't have to. "
+        'Make sure that you respond in English.'
+    ),
+    'form.system': (
+        'You will be provided with HTML page of an opportunity, and your task is to parse'
+        f'submit form from it. Resulting form must conform to this JSON schema: {SCHEMAS["form"]}.\n'
+        'Make sure that you respond in English.'
     ),
 }
 
@@ -31,10 +39,27 @@ async def query_opportunity_tags(client: AsyncOpenAI, opportunity_link: str, opp
             {'role': 'system', 'content': QUERY_MESSAGES['tags.system']},
             {'role': 'user', 'content': opportunity_page},
         ],
-        temperature=0.3,
-        # top_p=0.4,
+        temperature=0.25,
+        top_p=0.4,
         stream=False,
         extra_body={'nvext': {'guided_json': SCHEMAS['tags']}},
     )
     logger.info('Done querying tags for `%s`', opportunity_link)
+    return completion
+
+
+async def query_opportunity_form(client: AsyncOpenAI, opportunity_link: str, opportunity_page: str):
+    logger.info('Querying form for `%s`', opportunity_link)
+    completion = await client.chat.completions.create(
+        model=config.MODEL_NAME,
+        messages=[
+            {'role': 'system', 'content': QUERY_MESSAGES['form.system']},
+            {'role': 'user', 'content': opportunity_page},
+        ],
+        temperature=0.25,
+        top_p=0.4,
+        stream=False,
+        extra_body={'nvext': {'guided_json': SCHEMAS['form']}},
+    )
+    logger.info('Done querying form for `%s`', opportunity_link)
     return completion
