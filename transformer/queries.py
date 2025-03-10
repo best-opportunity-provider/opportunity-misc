74784920c37deb0c5811ed634de251ffb046bd6b
teviroff <mkaluzhnyy@mail.ru>
2025-03-10 14:23:25 +0300

    added & modified some queries

--- transformer/queries.py
+++ transformer/queries.py
@@ -14,6 +14,9 @@ def get_schema_file(filename: str):
 SCHEMAS = {
     'tags': get_schema_file('categorized-tags.schema.json').read(),
     'form': get_schema_file('form.schema.json').read(),
+    'general': get_schema_file('general.schema.json').read(),
+    'selection': get_schema_file('selection.schema.json').read(),
+    'details': get_schema_file('details.schema.json').read(),
 }
 
 QUERY_MESSAGES = {
@@ -24,9 +27,20 @@ QUERY_MESSAGES = {
         'Make sure that you respond in English.'
     ),
     'form.system': (
-        'You will be provided with HTML page of an opportunity, and your task is to parse'
-        f'submit form from it. Resulting form must conform to this JSON schema: {SCHEMAS["form"]}.\n'
-        'Make sure that you respond in English.'
+        'You will be provided with HTML page of an opportunity, and your task is to fill this JSON schema '
+        f'arrocding to it: {SCHEMAS["form"]}.\nMake sure that you respond in English.'
+    ),
+    'general.system': (
+        'You will be provided with HTML page of an opportunity, and your task is to fill this JSON schema '
+        f'according to it: {SCHEMAS["general"]}.\nMake sure that you respond in English.'
+    ),
+    'selection.system': (
+        'You will be provided with HTML page of an opportunity, and your task is to fill this JSON schema '
+        f'according to it: {SCHEMAS["selection"]}.\nBase your answer only on provided HTML.'
+    ),
+    'details.system': (
+        'You will be provided with HTML page of an opportunity, and your task is to fill this JSON schema '
+        f'according to it: {SCHEMAS["details"]}.\nMake sure that you respond in English.'
     ),
 }
 
@@ -57,9 +71,62 @@ async def query_opportunity_form(client: AsyncOpenAI, opportunity_link: str, opp
             {'role': 'user', 'content': opportunity_page},
         ],
         temperature=0.25,
-        top_p=0.4,
         stream=False,
         extra_body={'nvext': {'guided_json': SCHEMAS['form']}},
     )
     logger.info('Done querying form for `%s`', opportunity_link)
+    return completion
+
+
+async def query_general_opportunity_info(
+    client: AsyncOpenAI, opportunity_link: str, opportunity_page: str
+):
+    logger.info('Querying general info for `%s`', opportunity_link)
+    completion = await client.chat.completions.create(
+        model=config.MODEL_NAME,
+        messages=[
+            {'role': 'system', 'content': QUERY_MESSAGES['general.system']},
+            {'role': 'user', 'content': opportunity_page},
+        ],
+        temperature=0.25,
+        stream=False,
+        extra_body={'nvext': {'guided_json': SCHEMAS['general']}},
+    )
+    logger.info('Done querying general info for `%s`', opportunity_link)
+    return completion
+
+
+async def query_opportunity_selection_info(
+    client: AsyncOpenAI, opportunity_link: str, opportunity_page: str
+):
+    logger.info('Querying selection info for `%s`', opportunity_link)
+    completion = await client.chat.completions.create(
+        model=config.MODEL_NAME,
+        messages=[
+            {'role': 'system', 'content': QUERY_MESSAGES['selection.system']},
+            {'role': 'user', 'content': opportunity_page},
+        ],
+        temperature=0.25,
+        stream=False,
+        extra_body={'nvext': {'guided_json': SCHEMAS['selection']}},
+    )
+    logger.info('Done querying selection info for `%s`', opportunity_link)
+    return completion
+
+
+async def query_opportunity_details_info(
+    client: AsyncOpenAI, opportunity_link: str, opportunity_page: str
+):
+    logger.info('Querying details for `%s`', opportunity_link)
+    completion = await client.chat.completions.create(
+        model=config.MODEL_NAME,
+        messages=[
+            {'role': 'system', 'content': QUERY_MESSAGES['details.system']},
+            {'role': 'user', 'content': opportunity_page},
+        ],
+        temperature=0.25,
+        stream=False,
+        extra_body={'nvext': {'guided_json': SCHEMAS['details']}},
+    )
+    logger.info('Done querying details for `%s`', opportunity_link)
     return completion
